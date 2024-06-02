e37e2e98fbc9e24882ec61bec8ea7f4e8ae00400
shantinghou <68370542+shantinghou@users.noreply.github.com>
2024-06-02 12:30:19 -0400

    :ballot_box_with_check: reverse-linked-list [Runtime: 46 ms; Memory: 17.6 MB]

---
+++ linked-list/reverse-linked-list.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        last = head
+        if last == None:
+            return last
+        cur = head.next
+        last.next = None
+
+        while cur != None:
+            temp = cur
+            cur = cur.next
+            temp.next = last
+            last = temp
+        return last
